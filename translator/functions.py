cd1fbff6842133219fbca7c92758728e3f8e0959
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2023-10-29 01:50:38 +0200

    Add convert game input

--- translator/functions.py
+++ translator/functions.py
@@ -1,4 +1,4 @@
-from constants import NO_CARD, CARD_SUITS, REV_CARD_SUITS, CARD_VALUES, REV_CARD_VALUES
+from constants import NO_CARD, CARD_SUITS, CARD_VALUES, NUMBER_OF_CARDS, CARDS_SOURCE, CARDS_DEST
 
 import numpy as np
 
@@ -10,14 +10,6 @@ def convert_card_to_one_hot_encoded_vector(card):
     vector = list(map(int, list(suit + value)))
     return vector
 
-# Convert one hot encoded vector into card e.g. 01000000001000000 = 7C
-# Every digit of one hot encoded is stored separately
-def convert_one_hot_encoded_vector_to_card(vector):
-    value = ''.join(map(str, vector))
-    suit = REV_CARD_SUITS[value[:4]]
-    value = REV_CARD_VALUES[value[4:]]
-    return value + suit
-
 def convert_board_to_array_of_one_hot_encoded_values(board):
     # Fill entire array with no card symbol
     array_one_hot_encoded = np.full((8, 19, 17), fill_value=list(map(int, list(NO_CARD))))
@@ -28,7 +20,7 @@ def convert_board_to_array_of_one_hot_encoded_values(board):
 
     return array_one_hot_encoded
 
-def convert_free_cell_to_array_of_one_hot_encoded_values(free_cell):
+def convert_free_cells_to_array_of_one_hot_encoded_values(free_cell):
     array_one_hot_encoded = np.full((4, 17), fill_value=list(map(int, list(NO_CARD))))
     for i, card in enumerate(free_cell):
         array_one_hot_encoded[i] = convert_card_to_one_hot_encoded_vector(card)
@@ -42,9 +34,47 @@ def convert_heap_to_array_of_one_hot_encoded_values(heap):
 
     return array_one_hot_encoded
 
-def convert_to_game_vector(board, free_cell, heap):
+def convert_game_output_to_model_input(board, free_cells, heap):
     board = convert_board_to_array_of_one_hot_encoded_values(board)
-    free_cell = convert_free_cell_to_array_of_one_hot_encoded_values(free_cell)
+    free_cells = convert_free_cells_to_array_of_one_hot_encoded_values(free_cells)
     heap = convert_heap_to_array_of_one_hot_encoded_values(heap)
 
-    return board, free_cell, heap
+    return board, free_cells, heap
+
+# Get the card that should be moved for game input
+def get_source_card(board, free_cells, vector_no_cards, vector_source):
+    no_cards_move = NUMBER_OF_CARDS[''.join(map(str, vector_no_cards))]
+
+    conv_vector_source = CARDS_SOURCE[''.join(map(str, vector_source))]
+    source, no_col_source = conv_vector_source[0], int(conv_vector_source[1]) - 1
+
+    if source == 'F':
+        return free_cells[no_col_source]
+    elif source == 'C':
+        return board[no_col_source][-no_cards_move]
+    else:
+        raise Exception("Illegal source")
+
+# Get the card to which source card should be moved (or freecell or heap or empty column)
+def get_dest_card(board, vector_dest):
+    conv_vector_dest = CARDS_DEST[''.join(map(str, vector_dest))]
+    dest, no_col_dest = conv_vector_dest[0], int(conv_vector_dest[1]) - 1
+
+    if dest == 'S':
+        return "S"
+    elif dest == 'F':
+        return "F"
+    elif dest == 'C':
+        if len(board[no_col_dest]) == 0:
+            return '0'
+        else:
+            return board[no_col_dest][-1]
+    else:
+        raise Exception("Illegal destination")
+
+
+def convert_model_output_to_game_input(board, free_cells, vector_no_cards, vector_source, vector_dest):
+    source_card = get_source_card(board, free_cells, vector_no_cards, vector_source)
+    dest_card = get_dest_card(board, vector_dest)
+
+    return source_card, dest_card
--- translator/test.py
+++ translator/test.py
@@ -1,4 +1,4 @@
-from functions import convert_to_game_vector
+from functions import convert_game_output_to_model_input, convert_model_output_to_game_input
 
 test_board = [
     ["AH", "2H"],
@@ -11,15 +11,53 @@ test_board = [
     []
 ]
 
-test_freecell = [
+test_freecells = [
     "9S",
 ]
 
 test_heap = [
     "AC",
-    "AS",
-    "4C"
+    "AS"
 ]
 
-board, freecell, heap = convert_to_game_vector(test_board, test_freecell, test_heap)
-print(heap)
+test_no_cards = [
+    0,
+    1,
+    0,
+    0,
+    0
+]
+
+test_source = [
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+]
+
+test_destination = [
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+]
+
+a = convert_game_output_to_model_input(test_board, test_freecells, test_heap)
+
+d = convert_model_output_to_game_input(test_board, test_freecells, test_no_cards, test_source, test_destination)
+
+print(d)
