46601c3f2320b271b8e653f72f14ffc5128c5350
Kamil Krawiec <95848075+Kamil-Krawiec@users.noreply.github.com>
2023-11-10 08:52:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name="Translator FC",
+    name="Translator for games",
     version="0.0.1",
     author="Patryk, Mateusz, Wiktor, Kamil",
-    description="A dedicated translator for Freecell game",
+    description="Translator for games, now only freecell",
     packages=setuptools.find_packages(),
     python_requires=">=3.10",
     py_modules=["translator"],
